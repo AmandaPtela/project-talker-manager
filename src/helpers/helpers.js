fea4a626e39839abeb3338d398d5eadf01489bb6
Amanda Portela <mandaptela@gmail.com>
2023-01-14 20:38:28 -0300

    Requisito 8

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -28,8 +28,118 @@ function gerarToken() {
     return token;
 }
 
+// validações talker
+const auth = (_request, response, next) => {
+    const { authorization } = _request.headers;
+    if (!authorization) {
+      return response.status(401).json({ message: 'Token não encontrado' });
+    }
+    if (authorization.length !== 16 || typeof authorization !== 'string') {
+      return response.status(401).json({ message: 'Token inválido' });
+    }
+    next();
+  };
+  
+  const nameOk = (_request, response, next) => {
+    const { name } = _request.body;
+      if (!name) {
+        return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+      }
+      if (name.length < 3) {
+        return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+      }
+    next();
+  };
+  
+  const ageOk = (_request, response, next) => {
+    const { age } = _request.body;
+      if (!age) {
+        return response.status(400).json({ message: 'O campo "age" é obrigatório' });
+      }
+      if (age < 18) {
+        return response.status(400)
+        .json({ message: 'A pessoa palestrante deve ser maior de idade' });
+      }
+    next();
+  };
+  
+  const talkOk = (_request, response, next) => {
+    const { talk } = _request.body;
+    if (!talk) {
+      return response.status(400).json({ message: 'O campo "talk" é obrigatório' });
+    }
+    next();
+  };
+  
+  const talkKeys = (_request, response, next) => {
+    const { talk } = _request.body;
+    const { watchedAt } = talk;
+    const regexData = /\d{2}\/\d{2}\/\d{4}/gm;
+    
+    if (!watchedAt) {
+      return response.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+    }
+    if (!regexData.test(watchedAt)) {
+      return response.status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+    next();
+  };
+  const errorM = 'O campo "rate" deve ser um inteiro de 1 à 5'; 
+  const rateOk = (_request, response, next) => {
+    const { talk } = _request.body;
+    const { rate } = talk;
+    // const regexRate = /^[1-5]\d{0,5}$/;
+    const rateN = Number(rate);
+    if (!rate) {
+      return response.status(400).json({ message: 'O campo "rate" é obrigatório' });
+    }
+    if (!Number.isInteger(rateN)) {
+      return response.status(400).json({ message: `${errorM}` });
+    }
+    if (rateN < 1 || rateN > 5) {
+      return response.status(400).json({ message: `${errorM}` });
+    }
+    next();
+  };
+  
+  const rateOkPUT = (_request, response, next) => {
+    const { talk } = _request.body;
+    const { rate } = talk;
+    const rateN = Number(rate);
+    // const regexRate = /^[1-5]\d{0,5}$/;
+    if (rateN < 1) {
+      return response.status(400).json({ message: `${errorM}` });
+    }
+    if (rateN > 5) {
+      return response.status(400).json({ message: `${errorM}` });
+    }
+    if (!rateN) {
+      return response.status(400).json({ message: 'O campo "rate" é obrigatório' });
+    }
+    next();
+  };
+  
+  const rateOkPUT2 = (_request, response, next) => {
+    const { talk } = _request.body;
+    const { rate } = talk;
+    const rateN = Number(rate);
+    if (!Number.isInteger(rateN)) {
+      return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+    next();
+  };
+
 module.exports = {
     getTalkers,
     getUser,
     gerarToken,
+    auth,
+    nameOk,
+    ageOk,
+    talkOk,
+    talkKeys,
+    rateOk,
+    rateOkPUT,
+    rateOkPUT2,
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const fs = require('fs').promises;
-const { getTalkers, gerarToken } = require('./helpers/helpers');
+const {
+  getTalkers,
+  gerarToken,
+  auth,
+  nameOk,
+  ageOk,
+  talkOk,
+  talkKeys,
+  rateOk,
+  rateOkPUT,
+  rateOkPUT2,
+} = require('./helpers/helpers');
 
 const app = express();
 app.use(express.json());
@@ -13,21 +24,29 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send('ok');
 });
 
-// listar talkers
-app.get('/talker', async (_request, response) => {
+//  Listar talker específico
+app.get('/talker/search', auth, async (_request, response) => {
+  const { q } = _request.query;
   const talkers = await getTalkers();
-    response.status(HTTP_OK_STATUS).json(talkers);
+  const talker = talkers.filter((i) => i.name.toUpperCase().includes(q));
+
+  response.status(200).json(talker);
 });
 
-//  Listar talker específico
 app.get('/talker/:id', async (_request, response) => {
   const { id } = _request.params;
-  const data = await getTalkers();
-  const talker = data.find((i) => i.id === Number(id));
+  const talkers = await getTalkers();
+  const talker = talkers.find((i) => i.id === Number(id));
   if (!talker) {
     response.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   response.status(HTTP_OK_STATUS).json(talker);
+});
+
+// listar talkers
+app.get('/talker', async (_request, response) => {
+  const talkers = await getTalkers();
+    response.status(HTTP_OK_STATUS).json(talkers);
 });
 
 //  endpoint login
@@ -64,106 +83,6 @@ app.post('/login', validationLogin, (_request, _response) => {
   return _response.status(HTTP_OK_STATUS).json({ token });
 });
 
-// validações talker
-const auth = (_request, response, next) => {
-  const { authorization } = _request.headers;
-  if (!authorization) {
-    return response.status(401).json({ message: 'Token não encontrado' });
-  }
-  if (authorization.length !== 16 || typeof authorization !== 'string') {
-    return response.status(401).json({ message: 'Token inválido' });
-  }
-  next();
-};
-
-const nameOk = (_request, response, next) => {
-  const { name } = _request.body;
-    if (!name) {
-      return response.status(400).json({ message: 'O campo "name" é obrigatório' });
-    }
-    if (name.length < 3) {
-      return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-    }
-  next();
-};
-
-const ageOk = (_request, response, next) => {
-  const { age } = _request.body;
-    if (!age) {
-      return response.status(400).json({ message: 'O campo "age" é obrigatório' });
-    }
-    if (age < 18) {
-      return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-    }
-  next();
-};
-
-const talkOk = (_request, response, next) => {
-  const { talk } = _request.body;
-  if (!talk) {
-    return response.status(400).json({ message: 'O campo "talk" é obrigatório' });
-  }
-  next();
-};
-
-const talkKeys = (_request, response, next) => {
-  const { talk } = _request.body;
-  const { watchedAt } = talk;
-  const regexData = /\d{2}\/\d{2}\/\d{4}/gm;
-  
-  if (!watchedAt) {
-    return response.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
-  }
-  if (!regexData.test(watchedAt)) {
-    return response.status(400)
-    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
-  }
-  next();
-};
-const errorM = 'O campo "rate" deve ser um inteiro de 1 à 5'; 
-const rateOk = (_request, response, next) => {
-  const { talk } = _request.body;
-  const { rate } = talk;
-  // const regexRate = /^[1-5]\d{0,5}$/;
-  const rateN = Number(rate);
-  if (!rate) {
-    return response.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  }
-  if (!Number.isInteger(rateN)) {
-    return response.status(400).json({ message: `${errorM}` });
-  }
-  if (rateN < 1 || rateN > 5) {
-    return response.status(400).json({ message: `${errorM}` });
-  }
-  next();
-};
-
-const rateOkPUT = (_request, response, next) => {
-  const { talk } = _request.body;
-  const { rate } = talk;
-  const rateN = Number(rate);
-  // const regexRate = /^[1-5]\d{0,5}$/;
-  if (rateN < 1) {
-    return response.status(400).json({ message: `${errorM}` });
-  }
-  if (rateN > 5) {
-    return response.status(400).json({ message: `${errorM}` });
-  }
-  if (!rateN) {
-    return response.status(400).json({ message: 'O campo "rate" é obrigatório' });
-  }
-  next();
-};
-
-const rateOkPUT2 = (_request, response, next) => {
-  const { talk } = _request.body;
-  const { rate } = talk;
-  const rateN = Number(rate);
-  if (!Number.isInteger(rateN)) {
-    return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
-  next();
-};
 // criação de talker
 app.post('/talker', auth, nameOk, ageOk, talkOk, talkKeys, rateOk, async (_request, response) => {
   const reqBody = _request.body;
